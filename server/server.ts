110bb5e91c8c9d3a758815e51b1d33128388315d
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2024-08-12 22:02:50 +0530

    Add socket.io events to handle document updates and save in database

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import { connectDb } from './config/dbConfig';
+import { Document } from './models';
 
 const app: Express = express();
 const httpServer = createServer(app);
@@ -11,6 +12,31 @@ const io = new Server(httpServer, {
     methods: ['GET', 'POST'],
   },
 });
+
+io.on('connection', socket => {
+  socket.on('get-document', async documentId => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit('load-document', document?.data);
+
+    socket.on('send-changes', delta => {
+      socket.broadcast.to(documentId).emit('receive-changes', delta);
+    });
+
+    socket.on('save-document', async data => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
+
+async function findOrCreateDocument(id: string | null) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  const data = {};
+  return await Document.create({ _id: id, data });
+}
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
