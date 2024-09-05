d1923d537d179fd934267664827c9d8ec53e0214
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2024-09-05 21:22:53 +0530

    Add collaboration button to share the document link

--- packages/client/src/components/Header/Header.tsx
+++ packages/client/src/components/Header/Header.tsx
@@ -2,8 +2,10 @@ import MenubarComponent from '../Menubar/Menubar';
 import Logo from '/text-editor-icon.png';
 import Spinner from 'react-bootstrap/Spinner';
 import CloudIcon from '/cloud-icon.png';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { HeaderProps } from '../../types/types';
+import { Button, Toast } from 'react-bootstrap';
+import './Header.css';
 
 export default function Header({
   fileName,
@@ -13,54 +15,82 @@ export default function Header({
   socket,
   editor,
 }: HeaderProps) {
+  const [showToast, setShowToast] = useState(false);
+  const copyLink = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setShowToast(true);
+  };
+
   return (
-    <div style={{ display: 'flex', marginLeft: '1rem' }}>
+    <div style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
       <img
         src={Logo}
-        style={{ width: '40px', height: '40px', marginTop: '25px' }}
+        style={{ marginLeft: '15px', width: '40px', height: '40px' }}
       ></img>
       <div className="toolbar">
-        <input
-          type="text"
-          className="filename-input"
-          title="Rename"
-          style={{ width: `${fileName.length * 11}px` }}
-          value={fileName}
-          onChange={(e: ChangeEvent<HTMLInputElement>): void => {
-            setFileName(e.target.value);
-            setIsSaving(true);
-            socket?.emit(
-              'changes-to-server',
-              { fileName: e.target.value },
-              (response: { status: string; _id: string }) => {
-                if (response?.status === 'success') {
-                  setTimeout(() => {
-                    setIsSaving(false);
-                  }, 500);
+        <div style={{ display: 'flex', flexDirection: 'row', height: '20px' }}>
+          <input
+            type="text"
+            className="filename-input"
+            title="Rename"
+            style={{ width: `${fileName.length * 11}px` }}
+            value={fileName}
+            onChange={(e: ChangeEvent<HTMLInputElement>): void => {
+              setFileName(e.target.value);
+              setIsSaving(true);
+              socket?.emit(
+                'changes-to-server',
+                { fileName: e.target.value },
+                (response: { status: string; _id: string }) => {
+                  if (response?.status === 'success') {
+                    setTimeout(() => {
+                      setIsSaving(false);
+                    }, 500);
+                  }
                 }
-              }
-            );
-          }}
-        />
-        {isSaving ? (
-          <Spinner
-            animation="border"
-            role="status"
-            size="sm"
-            style={{ marginLeft: '10px' }}
+              );
+            }}
           />
-        ) : (
-          <img
-            src={CloudIcon}
-            style={{ width: '20px', height: '20px', marginLeft: '10px' }}
-          ></img>
-        )}
+          {isSaving ? (
+            <Spinner
+              animation="border"
+              role="status"
+              size="sm"
+              className="menubar-margin"
+            />
+          ) : (
+            <img
+              src={CloudIcon}
+              className="menubar-margin"
+              style={{ height: '20px' }}
+            ></img>
+          )}
+        </div>
         <MenubarComponent
           socket={socket}
           quill={editor?.quill}
           fileName={fileName}
         />
       </div>
+      <Toast
+        onClose={() => setShowToast(false)}
+        show={showToast}
+        delay={3000}
+        autohide
+      >
+        <Toast.Body>
+          Link copied! You can share the link with others to collaborate.
+        </Toast.Body>
+      </Toast>
+      <Button
+        size="sm"
+        onClick={copyLink}
+        title="Copy Link"
+        className="menubar-margin"
+        style={{ height: '30px', marginLeft: 'auto' }}
+      >
+        Collaborate
+      </Button>
     </div>
   );
 }
